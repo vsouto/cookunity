60ebf185291fc1fe25b47c4d0a6d5b4277dcffd8
Victor Souto <souto.victor@gmail.com>
2022-08-08 19:33:01 -0300

    webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './frontend/src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'frontend'),
+    },
+    compress: true,
+    port: 9000,
   },
 };
